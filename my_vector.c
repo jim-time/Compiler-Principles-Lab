71d88b557cd3f1e241008f11567d5fd56b0f4083
jim <lj200820082007@163.com>
2018-05-27 21:50:13 +0800

    complete vector

---
+++ my_vector.c
@@ -0,0 +1,73 @@
+#include "my_vector.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
+
+int vec_create(vec_t *vec){
+    if(vec == NULL){
+        printf("The vector is NULL!\n");
+        return 0;
+    }
+    vec->elem = (elem_type *)malloc(DEFAULT_CAPACITY*sizeof(elem_type));
+    if(vec->elem == NULL){
+        printf("Create vector failed\n");
+        return 0;
+    }
+    vec->capacity = DEFAULT_CAPACITY;
+    vec->size = 0;
+    return 1;
+}
+
+int vec_destruct(vec_t *vec){
+    if(vec == NULL){
+        printf("The vector is NULL!\n");
+        return 0;
+    }
+    free(vec->elem);
+    return 1;
+}
+
+int vec_expand(vec_t *vec){
+    if(vec->size == vec->capacity){
+        int iter;
+        elem_type* elem_old = vec->elem;
+        vec->elem = (elem_type *)malloc(2*vec->capacity*sizeof(elem_type));
+        for(iter = 0; iter < vec->capacity; iter++)
+            vec->elem[iter] = elem_old[iter];
+        vec->capacity <<=1;
+        free(elem_old);
+    }else
+        return 0;
+}
+
+int vec_push_back(vec_t *vec, elem_type * e){
+    vec->expand(vec);
+    vec->elem[vec->size++] = *e;
+    return 1;
+}
+
+int vec_pop(vec_t *vec, elem_type * e){
+    if(vec->size < 1){
+        printf("the size is zero\n");
+        return 0;
+    }else
+        *e = vec->elem[vec->size--];
+    return 1;
+}
+
+int vec_find(vec_t *vec,elem_type * e, elem_type* ret_e){
+    int iter;
+    if(vec->size < 1){
+        printf("vector size is zero\n");
+        return 0;
+    }
+    for(iter = vec->size-1; iter >= 0; iter--){
+        if(vec->isEqual(vec->elem + iter,e)){
+            ret_e = vec->elem + iter;
+            return 1;
+        }
+    }
+    ret_e = NULL;
+    return 0;
+}
--- my_vector.h
+++ my_vector.h
@@ -1,4 +1,33 @@
+#include "SymbolTable.h"
 #ifndef __MY_VECTOR_H_
 #define __MY_VECTOR_H_
+#include "stdio.h"
+
+//Variable Table Stack
+#define DEFAULT_CAPACITY 10
+typedef struct Vector_t vec_t;
+typedef struct VarTable_t elem_type;
+typedef struct VarTable_t* elem_ptr;
+
+struct Vector_t{
+    elem_type* elem;
+    int capacity;
+    int size;
+    int (*create)(vec_t *vec);
+    int (*destruct)(vec_t *vec);
+    int (*expand)(vec_t *vec);
+    int (*push_back)(vec_t *vec, elem_type * e);
+    int (*pop)(vec_t *vec, elem_type * e);
+    int (*find)(vec_t *vec,elem_type * e, elem_type* ret_e);
+    int (*isEqual)(elem_type *a, elem_type *b);
+};
+
+int vec_create(vec_t *vec);
+int vec_destruct(vec_t *vec);
+int vec_expand(vec_t *vec);
+int vec_push_back(vec_t *vec, elem_type * e);
+int vec_pop(vec_t *vec, elem_type * e);
+int vec_find(vec_t *vec,elem_type * e, elem_type* ret_e);
+//int vec_isEqual(elem_type* a, elem_type* b);
 
 #endif
